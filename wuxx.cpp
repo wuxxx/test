67cc8e63d196badf1eeed768584f6c7fc9bbf80e
wuxx-github <wuxiangxiang@wuxx-MBP.local>
2017-03-23 17:02:00 +0800

    just test
    just test

---
+++ wuxx.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int int main(int argc, char const *argv[]) {
+  cout << "hello  world" << endl;
+  //cout << "test" << endl;
+  int a;
+  int b;
+  return 0;
+}
